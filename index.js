905eb3c92e6b23812d5d0e04b6bcb4c0d013fb9f
Leonardo0622 <164056577+Leonardo0622@users.noreply.github.com>
2024-11-22 00:22:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); // Sirve archivos estáticos
+app.get('/', (req, res) => {
+    res.send('¡Hola, tu aplicación está corriendo!');
+});
 
 // Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI, {
